04772794f2ca278cd8afddaa8b0675f8d50a002c
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2021-06-28 12:19:40 +0200

    Update View.js

--- View.js
+++ View.js
@@ -2,7 +2,7 @@ show();
 function show() {
     document.getElementById('app').innerHTML = `
 
-        <input type="text" onchange="textValue = this.value" id="text" name="text"></input>
+        
         <button onclick="changeText()">textings</button><br/>
         ${response}
         
